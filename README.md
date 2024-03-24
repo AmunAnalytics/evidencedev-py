a0cc8fa5a1ddb3f8582e3ea56f74d59828368ac1
Frank Boerman <frank@fboerman.nl>
2024-03-24 22:27:55 +0100

    first setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# evidencedev
+
+This package is a small utility to download datasets from sites created in [evidence](https://evidence.dev/)
---
+++ evidencedev/evidencedev.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+from io import BytesIO
+
+
+__title__ = "evidencedev"
+__version__ = "0.1.0"
+__author__ = "Frank Boerman"
+__license__ = "MIT"
+
+
+class EvidenceClient:
+    def __init__(self, siteurl: str, auth_key: str = None, auth_header: str = 'X-API-Key'):
+        self.baseurl = siteurl.strip('/') + '/'
+
+        self.s = requests.Session()
+        self.s.headers.update({
+            'user-agent': f'evidencedev {__version__}'
+        })
+        if auth_key is not None:
+            self.s.headers.update({
+                auth_header: auth_key
+            })
+
+        r = self.s.get(self.baseurl + "data/manifest.json")
+        r.raise_for_status()
+        self.datasets = {}
+        for _, values in r.json()['renderedFiles']:
+            for value in values:
+                self.datasets[value.split('/')[-1].replace('.parquet', '')] = value
+
+    def list_datasets(self) -> list:
+        return list(self.datasets.keys())
+
+    def get_dataset(self, dataset: str) -> pd.DataFrame:
+        if dataset not in self.datasets:
+            raise Exception(f'Dataset {dataset} does not exist!')
+
+        r = self.s.get(self.datasets[dataset])
+        r.raise_for_status()
+        stream = BytesIO(r.content)
+        df = pd.read_parquet(stream, engine='pyarrow')
+
+        return df
